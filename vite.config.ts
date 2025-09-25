2e1be28085f2f551c7c1f65f542a87673cf98923
florian <git@florian.rgrplay.de>
2025-09-25 18:44:01 +0200

    fixed bug when replacing file with a larger one

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   base: "/webgme/",
   plugins: [
     tailwindcss(),
+    nodePolyfills(),
   ],
 })
